19270f1fa05eadb101ef80bc38200c8a564e1681
matty-bennett <bennyot10@gmail.com>
2021-07-10 15:57:26 -0600

    user created notes now save to the database and show on the notes page

--- server.js
+++ server.js
@@ -10,21 +10,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// get routes
+// function for creating note data
+function createNoteData(note) {
+    fs.writeFile('./db/db.json', JSON.stringify(note), (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('updated database');
+        return true;
+    })
+}
+
+
+//-- get routes --//
+
+// get notes page
 app.get('/notes', ( req, res ) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
+// get all notes
 app.get('/api/notes', ( req, res ) => {
     res.json(notes);
 })
 
+// get landing page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
-// api routes
 
+// create new note
+app.post('/api/notes', ( req,res ) => {
+    let newNote = req.body;
+
+    req.body.id = notes.length.toString();
+    notes.push(newNote);
+    console.log("Note has been created");
+    createNoteData(notes);
+    req.json(req.body);
+}) 
 
 
 app.listen(PORT, () => {
