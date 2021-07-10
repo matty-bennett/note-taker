a32fb4728c87d223cdb3ffe7336577cb8022e4dd
matty-bennett <bennyot10@gmail.com>
2021-07-10 14:18:06 -0600

    server runs but routes dont connect to html pages yet

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const notes = require('./db/db.json')
 
+// data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// html routes
+app.get('notes/', ( req, res ) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on port ${PORT}!`);
+})
